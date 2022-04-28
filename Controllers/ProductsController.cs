d7b20ebe18541ccd580b17b9486004896756f133
cfalco <carlesfalco97@gmail.com>
2022-04-28 13:07:33 +0200

    ListProducts + AddProducts

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Test.Models;
 
 namespace Test.Controllers
@@ -24,6 +23,97 @@ namespace Test.Controllers
             _productManagmentContext = context;
             _httpContextAccessor = httpContextAccessor;
         }
+
+        /// <summary>
+        /// Get all existing products
+        /// </summary>
+        /// <returns>List of Product</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        /// GET /ListProducts
+        ///
+        /// <response code="500">An error occurred listing products.</response>
+
+        [HttpGet]
+        public object ListProducts()
+        {
+            // Data to identify session and user. At this moment we only have IP
+            string IP = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            _logger.LogInformation($"[ListProducts-{IP}] Getting prodcuts...");
+            var productsList = new List<Product>();
+
+            try
+            {
+                productsList = _productManagmentContext.Products.ToList();
+                if (productsList.Count == 0)
+                    _logger.LogDebug($"[ListProducts-{IP}] No products found.");
+
+
+                return productsList;
+            }
+            catch (System.Exception e)
+            {
+                var error = $"An error occurred listing products: {e.Message}.";
+
+                _logger.LogError($"[ListProducts-{IP}] Unable to get products. Error: {error}");
+
+                return StatusCode(500, productsList);
+            }
+        }
+
+        /// <summary>
+        /// Adds new Product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns>ActionResult</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        /// POST /AddProduct
+        ///{
+        ///   "name": "string",
+        ///   "description": "string",
+        ///   "price": 0.00,
+        ///   "familyProduct": "string"
+        /// },
+        ///
+        /// </remarks>
+        /// <response code="200">OK if product added. </response>
+        /// <response code="400">OK if product is not valid. </response>
+        /// <response code="500">OK if error occurred adding product. </response>
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            string IP = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            _logger.LogInformation($"[AddProduct-{IP}] Adding product {JsonConvert.SerializeObject(product)}...");
+
+            // Forcing id to 0 because we don't know what we have. To ensure integrity.
+            product.Id = 0;
+
+            // Model validation to ensure the integrity of the object. In porducts as DevExpress we can use decarators to do this task automaically.
+            var (valid, message) = product.IsValid();
+            if (!valid)
+                return StatusCode(400, message);
+
+            _productManagmentContext.Add(product);
+
+            try
+            {
+                _productManagmentContext.SaveChanges();
+            }
+            catch (System.Exception e)
+            {
+                var error = $"An error occurred adding product: {e.Message}.";
+
+                _logger.LogError($"[AddProduct-{IP}] {JsonConvert.SerializeObject(product)} Error: {error}");
+
+                return StatusCode(500, error);
+            }
+
+            return Ok();
         }
 
         /// <summary>
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Test.Models
 {
-    public class ErrorViewModel
+    public class Product
     {
-        public string RequestId { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public string FamilyProduct { get; set; }
+ 
+        public (bool, string) IsValid()
+        {
+            List<string> messages = new();
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+            if (string.IsNullOrEmpty(Name))
+                messages.Add("Name cannot be empty.");
+
+            if (Price < 0m)
+                messages.Add("Price cannot be negative.");
+
+            return (messages.Count == 0, string.Join("\r\n", messages));
+        }
     }
 }
