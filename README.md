95ba23fd32a40fd68636d0eb5f1b854167f96542
cfalco <carlesfalco97@gmail.com>
2022-04-29 12:40:25 +0200

    Unit test + readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Test
+
+Proyecto ASP.NET Core Web App MVC en .NET 5 con Enity Framework 5.0. Se ha usado IDE VS22.
+
+Dada la facilidad de uso del ejecutbale de .NET Core se ignora la ejecuión del IIS express y se usa como stand alone.
+Al ejecutar la aplicación pedirá la instalación de los certificados. Aceptamos.
+
+El proyecto necesita de una conexión a una BD SQL Express "Model". Esta BD necesita la tabla "Products".
+Se adjunta al proyecto una copia de la BD.
+Cadena de conexión para la BD: "Server=localhost\SQLEXPRESS;Database=model;Trusted_Connection=True;"
+No se usa autentificación ya que no es necesaria para este test.
+
+Script para la tabla "Products", por si no se quiere restaurar la copia de BD:
+
+''''
+	CREATE TABLE [dbo].[Products](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NOT NULL,
+	[description] [varchar](max) NULL,
+	[price] [decimal](18, 2) NOT NULL,
+	[FamilyProduct] [varchar](255) NULL,
+	PRIMARY KEY CLUSTERED 
+	(
+		[id] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+	GO
+''''
+
+SWAGGER
+Este proyecto contiene Swagger. Para viusalizarlo acceder a la url "https://localhost:5001/swagger".
+
+UNIT TEST
+Este proyecto contiene Unit Test. Se ha usado NUnit para la ejeccución de los test. Estos se encuentran en otro proyecto (UnitTest) dentro de la misma solución. 
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
 using NUnit.Framework;
+using Test;
+using Test.Controllers;
+using Test.Models;
 
 namespace UnitTest
 {
@@ -7,12 +15,84 @@ namespace UnitTest
         [SetUp]
         public void Setup()
         {
+           
         }
 
         [Test]
-        public void Test1()
+        public void ProductModel()
         {
-            Assert.Pass();
+            Product product = new() 
+            { 
+                Name = "",
+                Description = "test description",
+                FamilyProduct = "test family",
+                Price = 10m
+            };
+
+            var (isValid, message) = product.IsValid();
+
+            Assert.IsFalse(isValid);
+            Assert.AreEqual("Name cannot be empty.", message);
+
+            product.Name = "test product";
+            product.Price = -10m;
+
+            (isValid, message) = product.IsValid();
+
+            Assert.IsFalse(isValid);
+            Assert.AreEqual("Price cannot be negative.", message);
+
+            product.Name = "";
+
+            (isValid, message) = product.IsValid();
+
+            Assert.IsFalse(isValid);
+            Assert.AreEqual("Name cannot be empty.\r\nPrice cannot be negative.", message);
+
+            product.Name = "test product";
+            product.Price = 10m;
+
+            (isValid, message) = product.IsValid();
+
+            Assert.IsTrue(isValid);
+            Assert.IsEmpty(message);
+        }
+
+        [Test]
+        public void Views()
+        {
+            var controller = new HomeController();
+
+            var result = controller.AddProduct() as ViewResult;
+            Assert.AreEqual("AddProduct", result.ViewName);
+
+            result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [Test]
+        public void ProductController()
+        {
+            // We declare this variables inside the test because we don't want this to effect other scopes. Otherwise this would be on Setup() with global variables.
+            var mockLogger = new Mock<ILogger<ProductsController>>();
+            var options = new DbContextOptionsBuilder<ProductManagmentContext>()
+             .UseInMemoryDatabase(databaseName: "ProductDB")
+             .Options;
+
+            var mockProductManagmentContext = new Mock<ProductManagmentContext>(options);
+            var mockContextAccessor = new Mock<IHttpContextAccessor>();
+            var controller = new ProductsController(mockLogger.Object, mockProductManagmentContext.Object, mockContextAccessor.Object);
+
+            var product = new Product() { Name = "test" };
+
+            var resultOk = controller.Add(product) as OkResult;
+            Assert.AreEqual(resultOk.StatusCode, 200);
+
+            product.Name = "";
+
+            var result1 = controller.Add(product) as ObjectResult;
+            Assert.AreEqual(result1.StatusCode, 400);
+            Assert.AreEqual(result1.Value, "Name cannot be empty.");
         }
     }
 }
